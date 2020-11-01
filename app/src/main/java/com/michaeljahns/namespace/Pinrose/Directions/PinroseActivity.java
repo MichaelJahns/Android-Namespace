d14c77ffad58e4403bdb966fbbf6399d676837a3
MichaelJahns <jahnsmic001@gmail.com>
2020-11-01 07:47:00 -0800

    NorthWest built and wired

--- app/src/main/java/com/michaeljahns/namespace/Pinrose/Directions/PinroseActivity.java
+++ app/src/main/java/com/michaeljahns/namespace/Pinrose/Directions/PinroseActivity.java
@@ -1,4 +1,4 @@
-package com.michaeljahns.namespace;
+package com.michaeljahns.namespace.Pinrose.Directions;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,6 +8,8 @@ import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+
+import com.michaeljahns.namespace.R;
 
 public class PinroseActivity extends Fragment {
 
