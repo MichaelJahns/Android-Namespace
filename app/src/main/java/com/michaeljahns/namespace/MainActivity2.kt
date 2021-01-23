aa7567f1773fe840646d5805ef97aae07c36d8d0
MichaelJahns <jahnsmic001@gmail.com>
2021-01-23 11:52:17 -0800

    Writing my first real code with Kotlin, transfering my old java fxn by fxn trying to understand the differences, working to shit old essential code to kotlin

--- app/src/main/java/com/michaeljahns/namespace/MainActivity2.kt
+++ app/src/main/java/com/michaeljahns/namespace/MainActivity2.kt
@@ -1,9 +1,16 @@
 package com.michaeljahns.namespace
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import com.michaeljahns.namespace.grammy.Location
+import com.michaeljahns.namespace.grammy.LocationLayoutAdapter
+import com.michaeljahns.namespace.grammy.Tracery
+import java.util.*
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -15,5 +22,42 @@ class MainActivity2 : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+        val context = GlobalApplication.getAppContext()
+        val JSON = readJsonFromAsset(context)
+        var locations: List<Location> = flattenLocationsFromJson(JSON)
+        startRecycler(locations, context)
+    }
+
+
+    fun flattenLocationsFromJson(JSON: String): List<Location> {
+        val locations: MutableList<Location> = ArrayList()
+        for (i in 1..15) {
+            var locationName = Tracery.flattenJSON(JSON)
+            val location = Location(locationName)
+            locations.add(location)
+        }
+        return locations
+    }
+
+    fun readJsonFromAsset(context: Context): String? {
+        var json: String? = null
+        json = try {
+            val inputStream = context.assets.open("pirateLocations.json")
+            val buffer = ByteArray(inputStream.available())
+            inputStream.read(buffer)
+            inputStream.close()
+            String(buffer)
+        } catch (e: Exception) {
+            Log.e("Error", e.message)
+            return null
+            return json
+        }
+        return null;
+    }
+
+    fun startRecycler(locations: List<Location?>?, context: Context?) {
+        val locationLayoutAdapter = LocationLayoutAdapter(context, locations)
+        this.locationRecycler.setAdapter(locationLayoutAdapter)
+        this.locationRecycler.setLayoutManager(LinearLayoutManager(context))
     }
 }
