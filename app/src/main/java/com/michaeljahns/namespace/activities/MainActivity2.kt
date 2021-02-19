5cd8a0f4d39696a3ec7d1f3bafa4e4266ace656f
MichaelJahns <jahnsmic001@gmail.com>
2021-02-19 09:27:14 -0800

    Got the omnifabs onclick listener to be automatically changed as the active view changes, rearranges Main a bit to reuse code, abstracts setOmniFapOnClick fxn to bindOmniFab and adds a when loop to decide when onclick to set it as, adds the scenarioModel and the Omnifap to private lateinit vars

--- app/src/main/java/com/michaeljahns/namespace/activities/MainActivity2.kt
+++ app/src/main/java/com/michaeljahns/namespace/activities/MainActivity2.kt
@@ -1,11 +1,13 @@
 package com.michaeljahns.namespace.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.michaeljahns.namespace.R
 import com.michaeljahns.namespace.databinding.ActivityMainBinding
 import com.michaeljahns.namespace.fragments.CollectionFragment
@@ -24,37 +26,66 @@ class MainActivity2 : AppCompatActivity() {
     private val forageFragment = ForageFragment()
     private val collectionFragment = CollectionFragment()
     private val model: UIViewModel by viewModels()
-    private val scenarioModel: ScenarioModel by viewModels()
+    private lateinit var viewModel: ScenarioModel
+    private lateinit var omniFab: FloatingActionButton
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val factory = InjectorUtils.provideScenarioModelFactory()
-        val viewModel = ViewModelProvider(this, factory)
+        viewModel = ViewModelProvider(this, factory)
                 .get(ScenarioModel::class.java)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.mainNavigationView, navigationFragment)
             commit()
         }
-        binding.mainNavigationView.omnifab.setOnClickListener {
-            viewModel.regenerateScenarios()
-        }
-
+        omniFab = binding.mainNavigationView.omnifab
         setCurrentFragment(scenarioFragment)
+        model.setActiveViewString("Scenario")
+        bindOmniFab()
         model.intView.observe(this, Observer {
             when (it) {
                 R.id.miScenario -> {
                     setCurrentFragment(scenarioFragment)
+                    model.setActiveViewString("Scenario")
+//                    namespaceOrange
                 }
-                R.id.miCollection -> setCurrentFragment(collectionFragment)
-//                R.id.miSettings -> setCurrentFragment(settingsFragment)
-                R.id.miForage -> setCurrentFragment(forageFragment)
+                R.id.miForage -> {
+                    setCurrentFragment(forageFragment)
+                    model.setActiveViewString("Forage")
+//                    forageGreen
+                }
+                R.id.miCollection -> {
+                    setCurrentFragment(collectionFragment)
+                    model.setActiveViewString("Collection")
+                }
+
             }
+            bindOmniFab()
         })
         val view = binding.root
         setContentView(view)
+    }
+
+    private fun bindOmniFab() {
+        omniFab.setOnClickListener {
+            when (model.activeViewString.value) {
+                "Scenario" -> {
+                    viewModel.regenerateScenarios()
+                    Log.d("MAIN", "Omnifab onclick to reset generated scenarios")
+
+                }
+                "Forage" -> {
+
+                    Log.d("MAIN", "Omnifab onclick to reset generated Forages")
+                }
+                "Collection" -> {
+                    
+                    Log.d("MAIN", "Omnifab onclick to appropriate collection action")
+                }
+            }
+        }
     }
 
     private fun setCurrentFragment(fragment: Fragment) =
@@ -62,6 +93,8 @@ class MainActivity2 : AppCompatActivity() {
                 replace(R.id.mainFrameLayout, fragment)
                 commit()
             }
+
+
 }
 
 
--- app/src/main/java/com/michaeljahns/namespace/models/UIViewModel.kt
+++ app/src/main/java/com/michaeljahns/namespace/models/UIViewModel.kt
@@ -5,7 +5,12 @@ import androidx.lifecycle.ViewModel
 
 class UIViewModel : ViewModel() {
     var intView = MutableLiveData<Int>()
+    var activeViewString = MutableLiveData<String>()
     fun select(int: Int) {
         intView.value = int
+    }
+
+    fun setActiveViewString(string: String) {
+        activeViewString.value = string
     }
 }
