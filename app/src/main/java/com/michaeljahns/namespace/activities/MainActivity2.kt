2c46cf5e45f96a31e0fff5e974e0b275e3c22434
MichaelJahns <jahnsmic001@gmail.com>
2021-02-19 10:10:12 -0800

    Attempts to programmatically update the Fabs color based on the current view, I couldnt figure out how to change backgroundTint so i settled for now on image Icon and changed the omniFabs backgrounColor so namespaceOrange isnt drowned out, also I wasnt able to figure out how to reference my own colors, so for now the colors are being entered with rgb

--- app/src/main/java/com/michaeljahns/namespace/activities/MainActivity2.kt
+++ app/src/main/java/com/michaeljahns/namespace/activities/MainActivity2.kt
@@ -1,7 +1,10 @@
 package com.michaeljahns.namespace.activities
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -20,27 +23,28 @@ import com.michaeljahns.namespace.util.InjectorUtils
 
 class MainActivity2 : AppCompatActivity() {
     private val navigationFragment = NavigationFragment()
-    private val scenarioFragment = ScenarioFragment()
 
-    //    private val settingsFragment = SettingsFragment()
+    private val scenarioFragment = ScenarioFragment()
     private val forageFragment = ForageFragment()
     private val collectionFragment = CollectionFragment()
+
     private val model: UIViewModel by viewModels()
-    private lateinit var viewModel: ScenarioModel
-    private lateinit var omniFab: FloatingActionButton
+    private lateinit var scenarioModel: ScenarioModel
+
     lateinit var binding: ActivityMainBinding
+    private lateinit var Fab: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val factory = InjectorUtils.provideScenarioModelFactory()
-        viewModel = ViewModelProvider(this, factory)
+        scenarioModel = ViewModelProvider(this, factory)
                 .get(ScenarioModel::class.java)
         binding = ActivityMainBinding.inflate(layoutInflater)
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.mainNavigationView, navigationFragment)
             commit()
         }
-        omniFab = binding.mainNavigationView.omnifab
+        Fab = binding.mainNavigationView.omnifab
         setCurrentFragment(scenarioFragment)
         model.setActiveViewString("Scenario")
         bindOmniFab()
@@ -49,18 +53,18 @@ class MainActivity2 : AppCompatActivity() {
                 R.id.miScenario -> {
                     setCurrentFragment(scenarioFragment)
                     model.setActiveViewString("Scenario")
-//                    namespaceOrange
+                    Fab.imageTintList = ColorStateList.valueOf(Color.rgb(255, 153, 15))
                 }
                 R.id.miForage -> {
                     setCurrentFragment(forageFragment)
                     model.setActiveViewString("Forage")
-//                    forageGreen
+                    Fab.imageTintList = ColorStateList.valueOf(Color.rgb(41, 197, 29))
                 }
                 R.id.miCollection -> {
                     setCurrentFragment(collectionFragment)
                     model.setActiveViewString("Collection")
+                    Fab.imageTintList = ColorStateList.valueOf(Color.rgb(40, 44, 52))
                 }
-
             }
             bindOmniFab()
         })
@@ -69,19 +73,19 @@ class MainActivity2 : AppCompatActivity() {
     }
 
     private fun bindOmniFab() {
-        omniFab.setOnClickListener {
+        Fab.setOnClickListener {
             when (model.activeViewString.value) {
                 "Scenario" -> {
-                    viewModel.regenerateScenarios()
+                    scenarioModel.regenerateScenarios()
                     Log.d("MAIN", "Omnifab onclick to reset generated scenarios")
-
                 }
                 "Forage" -> {
-
+                    Toast.makeText(this, "Future Feature, Regenerate Forages!", Toast.LENGTH_LONG).show()
+//                    forageModel.regenerateScenarios()
                     Log.d("MAIN", "Omnifab onclick to reset generated Forages")
                 }
                 "Collection" -> {
-                    
+                    Toast.makeText(this, "Future Feature, add to your collection!", Toast.LENGTH_LONG).show()
                     Log.d("MAIN", "Omnifab onclick to appropriate collection action")
                 }
             }
@@ -93,8 +97,6 @@ class MainActivity2 : AppCompatActivity() {
                 replace(R.id.mainFrameLayout, fragment)
                 commit()
             }
-
-
 }
 
 
